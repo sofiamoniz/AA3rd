d553f0c675d3726e5b0161ef9edda25e18e098b2
sofiamoniz <sofiamoniz@ua.pt>
2021-01-21 11:52:28 +0000

    new

--- code/Main.py
+++ code/Main.py
@@ -1,7 +1,7 @@
 from Counters.Exact_counter import Exact_counter
 from Counters.count_min_sketch import CountMinSketch
 
-def main(file_to_read, columns=50, rows=5):
+def main(file_to_read):
     ###################
     # Create exact counter #
     ###################
@@ -16,15 +16,20 @@ def main(file_to_read, columns=50, rows=5):
     ###################
     # Create count min sketch #
     ###################
-
-    min_sketch = CountMinSketch(file_to_read, m=columns, d=rows)
-    hash_functions = ['md5', 'sha256', 'sha1', 'blake2s', 'dsaEncryption']
-    for hash_func in hash_functions:
-        min_sketch.count_chars(exact_counter_result, hash_function=hash_func)
-        min_sketch_result = min_sketch.get_top_20_chars()
-        print(min_sketch_result)
-        print(min_sketch.get_total_counted_chars())
-        min_sketch.clear()
+    columns = [50,40,20,30,10]
+    rows = [5,4,3,2,1]
+    for col in columns:
+        for row in rows:
+            min_sketch = CountMinSketch(file_to_read, m=col, d=row)
+            hash_functions = ['md5', 'sha256', 'sha1', 'blake2s', 'dsaEncryption']
+            print("col ", col," row ",row)
+            for hash_func in hash_functions:
+                print("hash ", hash_func)
+                min_sketch.count_chars(exact_counter_result, hash_function=hash_func)
+                min_sketch_result = min_sketch.get_top_20_chars()
+                print(min_sketch_result)
+                print(min_sketch.get_total_counted_chars())
+                min_sketch.clear()
 
 
     
